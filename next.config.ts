a4bcf36111c406bca9bd5645301c9d15589dd16b
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-04-11 05:09:05 +0530

    MDX Setup removed

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,9 @@
 import type { NextConfig } from "next"
-import createMDX from "@next/mdx"
 
 const nextConfig: NextConfig = {
   trailingSlash: true,
   reactStrictMode: true,
-  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  pageExtensions: ["js", "jsx", "ts", "tsx"],
 }
 
-const withMDX = createMDX({
-  options: {},
-})
-
-export default withMDX(nextConfig)
+export default nextConfig
