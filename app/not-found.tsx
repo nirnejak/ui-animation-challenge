eca741dff4a061664c7165a93ca5b61956d0eadd
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-04-12 07:21:33 +0530

    404 page updated

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,5 @@
 import * as React from "react"
 
-import { Link } from "next-view-transitions"
-
 import { getMetadata } from "@/utils/metadata"
 
 export const metadata = getMetadata({
@@ -12,23 +10,8 @@ export const metadata = getMetadata({
 
 const NotFound: React.FC = () => {
   return (
-    <main className="container">
-      <section className="grid min-h-dvh place-content-center">
-        <div className="text-center">
-          <h1 className="mb-3 text-5xl font-bold tracking-tighter text-zinc-800 dark:text-zinc-300">
-            404
-          </h1>
-          <p className="text-base text-zinc-800 dark:text-zinc-300">
-            The page you&apos;re looking for cannot be found. Go{" "}
-            <Link
-              href="/"
-              className="font-semibold hover:underline focus:underline"
-            >
-              Home
-            </Link>
-          </p>
-        </div>
-      </section>
+    <main className="mt-36 text-center">
+      <h1 className="mb-3 text-5xl tracking-tight text-zinc-800">404</h1>
     </main>
   )
 }
