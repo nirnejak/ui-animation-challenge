b166079f9368cedcf2573c1595fc3b1a8b9e49d1
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-04-12 07:34:45 +0530

    Message component: text scramble

--- components/Message.tsx
+++ components/Message.tsx
@@ -1,5 +1,7 @@
 import * as React from "react"
 
+import { TextScramble } from "@/components/text-scramble"
+
 interface Props {
   message: string
 }
@@ -8,7 +10,9 @@ const Message: React.FC<Props> = ({ message }) => {
   return (
     <main className="mt-6 -mx-3.5 md:mx-0 md:mt-10 rounded-xl bg-zinc-50 border border-zinc-200">
       <div className="h-[55vh] grid place-content-center">
-        <p className="text-zinc-800 tracking-tight">{message}</p>
+        <TextScramble className="text-zinc-800 tracking-tight">
+          {message}
+        </TextScramble>
       </div>
     </main>
   )
