a6c0f83eea43497ce1c83fa3b0a3cbd87ab1d586
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-04-12 06:10:14 +0530

    Header: accessibility improved

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,16 @@
+"use client"
 import * as React from "react"
 
-import { TextScramble } from "@/components/text-scramble"
 import { Link } from "next-view-transitions"
+import { usePathname } from "next/navigation"
+
+import classNames from "@/utils/classNames"
+
+import { TextScramble } from "@/components/text-scramble"
 
 const Header: React.FC = () => {
+  const pathname = usePathname()
+
   return (
     <header className="grid grid-cols-1 md:grid-cols-2">
       <section className="flex flex-col justify-between">
@@ -26,23 +33,65 @@ const Header: React.FC = () => {
         </div>
       </section>
       <section className="flex flex-col gap-2 text-sm md:text-base mt-20 md:mt-0">
-        <Link href="/day-1/" className="text-zinc-800">
-          <span className="text-zinc-500">Day 1 </span>- Fluid Menu Animation
+        <Link
+          href="/day-1/"
+          className={classNames(
+            "text-zinc-800",
+            pathname !== "/" && "opacity-35 hover:opacity-100 transition-all",
+            pathname === "/day-1/" ? "opacity-100" : ""
+          )}
+        >
+          <span className="">Day 1 </span>- Fluid Menu Animation
         </Link>
-        <Link href="/day-2/" className="text-zinc-800">
-          <span className="text-zinc-500">Day 2 </span>- Coming Soon
+        <Link
+          href="/day-2/"
+          className={classNames(
+            "text-zinc-800",
+            pathname !== "/" && "opacity-35 hover:opacity-100 transition-all",
+            pathname === "/day-2/" ? "opacity-100" : ""
+          )}
+        >
+          <span className="">Day 2 </span>- Coming Soon
         </Link>
-        <Link href="/day-3/" className="text-zinc-800">
-          <span className="text-zinc-500">Day 3 </span>- Coming Soon
+        <Link
+          href="/day-3/"
+          className={classNames(
+            "text-zinc-800",
+            pathname !== "/" && "opacity-35 hover:opacity-100 transition-all",
+            pathname === "/day-3/" ? "opacity-100" : ""
+          )}
+        >
+          <span className="">Day 3 </span>- Coming Soon
         </Link>
-        <Link href="/day-4/" className="text-zinc-800">
-          <span className="text-zinc-500">Day 4 </span>- Coming Soon
+        <Link
+          href="/day-4/"
+          className={classNames(
+            "text-zinc-800",
+            pathname !== "/" && "opacity-35 hover:opacity-100 transition-all",
+            pathname === "/day-4/" ? "opacity-100" : ""
+          )}
+        >
+          <span className="">Day 4 </span>- Coming Soon
         </Link>
-        <Link href="/day-5/" className="text-zinc-800">
-          <span className="text-zinc-500">Day 5 </span>- Coming Soon
+        <Link
+          href="/day-5/"
+          className={classNames(
+            "text-zinc-800",
+            pathname !== "/" && "opacity-35 hover:opacity-100 transition-all",
+            pathname === "/day-5/" ? "opacity-100" : ""
+          )}
+        >
+          <span className="">Day 5 </span>- Coming Soon
         </Link>
-        <Link href="/day-6/" className="text-zinc-800">
-          <span className="text-zinc-500">Day 6 </span>- Coming Soon
+        <Link
+          href="/day-6/"
+          className={classNames(
+            "text-zinc-800",
+            pathname !== "/" && "opacity-35 hover:opacity-100 transition-all",
+            pathname === "/day-6/" ? "opacity-100" : ""
+          )}
+        >
+          <span className="">Day 6 </span>- Coming Soon
         </Link>
       </section>
     </header>
