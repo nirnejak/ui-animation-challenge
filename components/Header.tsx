3a69dc9cb59cac87e7926d64eaa8bd63dd4e8586
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-04-12 06:16:01 +0530

    Header simplified

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,24 +15,22 @@ const Header: React.FC = () => {
     <header className="grid grid-cols-1 md:grid-cols-2">
       <section className="flex flex-col justify-between">
         <Link href="/">
-          <h1 className="text-2xl text-zinc-800">
+          <h1 className="text-xl md:text-2xl text-zinc-800">
             <TextScramble className="uppercase">Peerlist</TextScramble>
             <TextScramble className="uppercase">UI Animation</TextScramble>
             <TextScramble className="uppercase">Challenge</TextScramble>
           </h1>
         </Link>
 
-        <div className="fixed bottom-4 right-0 md:static flex gap-6 justify-center md:justify-start w-full">
-          <a
-            href="https://x.com/jeetnirnejak"
-            target="_blank"
-            className="text-zinc-500 hover:text-zinc-800 transition-colors text-xs md:text-base"
-          >
-            <TextScramble>by Jitendra Nirnejak</TextScramble>
-          </a>
-        </div>
+        <a
+          href="https://x.com/jeetnirnejak"
+          target="_blank"
+          className="text-zinc-500 hover:text-zinc-800 transition-colors text-xs md:text-base hidden md:block"
+        >
+          <TextScramble>by Jitendra Nirnejak</TextScramble>
+        </a>
       </section>
-      <section className="flex flex-col gap-2 text-sm md:text-base mt-20 md:mt-0">
+      <section className="flex flex-col gap-2 text-sm md:text-base mt-12 md:mt-0">
         <Link
           href="/day-1/"
           className={classNames(
