266afbd4276510540025c6506ee03748fbfd32fb
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-04-14 07:04:11 +0530

    AnimatedCheckboxes initial version

--- components/AnimatedCheckboxes.tsx
+++ components/AnimatedCheckboxes.tsx
@@ -1,10 +1,25 @@
 "use client"
 import * as React from "react"
 
-interface Props {}
+import Checkbox from "@/components/Checkbox"
 
-const AnimatedCheckboxes: React.FC<Props> = () => {
-  return <div></div>
+const AnimatedCheckboxes = () => {
+  return (
+    <section className="space-y-3 font-inter tracking-tight">
+      <Checkbox id="c-1">
+        <Checkbox.Indicator />
+        <Checkbox.Label>Buy Groceries</Checkbox.Label>
+      </Checkbox>
+      <Checkbox id="c-2">
+        <Checkbox.Indicator />
+        <Checkbox.Label>Contemplate existence</Checkbox.Label>
+      </Checkbox>
+      <Checkbox id="c-3">
+        <Checkbox.Indicator />
+        <Checkbox.Label>Learn Swift UI</Checkbox.Label>
+      </Checkbox>
+    </section>
+  )
 }
 
 export default AnimatedCheckboxes
