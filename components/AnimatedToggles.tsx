3709445e477bff9faa91eb29e53e06b86cb2d346
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-04-15 10:16:14 +0530

    Animation values adjusted

--- components/AnimatedToggles.tsx
+++ components/AnimatedToggles.tsx
@@ -70,7 +70,7 @@ const AnimatedToggles: React.FC = () => {
             <motion.div
               initial={{ opacity: 0, scale: 0.8 }}
               animate={{ opacity: 1, scale: 1 }}
-              exit={{ opacity: 0, scale: 0.8, transition: { duration: 0.2 } }}
+              exit={{ opacity: 0, scale: 0.8 }}
               transition={{
                 duration: 0.3,
                 ease: "easeOut",
