ef85261a25f71e7bbdabdf85fd16520732247257
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-04-16 05:32:06 +0530

    SharedLayoutTabs: empty component added

--- app/day-5/page.tsx
+++ app/day-5/page.tsx
@@ -2,21 +2,19 @@ import * as React from "react"
 
 import { getMetadata } from "@/utils/metadata"
 
-import { TextScramble } from "@/components/atoms/text-scramble"
+import SharedLayoutTabs from "@/components/SharedLayoutTabs"
 
 export const metadata = getMetadata({
   path: "/day-5/",
-  title: "Day 5 - UI Animation Challenge",
-  description: "Day 5 - UI Animation Challenge",
+  title: "Day 5 - Shared Layout Tabs",
+  description: "Day 5 - Shared Layout Tabs, Peerlist UI Animation Challenge",
 })
 
 const Day5: React.FC = () => {
   return (
     <main className="mt-6 -mx-3.5 md:mx-0 md:mt-10 rounded-xl bg-zinc-50 border border-zinc-200">
       <div className="h-[55vh] grid place-content-center">
-        <TextScramble className="text-zinc-800 tracking-tight">
-          Not yet announced
-        </TextScramble>
+        <SharedLayoutTabs />
       </div>
     </main>
   )
