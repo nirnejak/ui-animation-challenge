6cb0de3b44b9584113e93f567790cad50111676b
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-05-02 18:48:48 +0530

    View Source Link

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import * as React from "react"
 
+import { Link } from "next-view-transitions"
+
 import { getMetadata } from "@/utils/metadata"
 
 export const metadata = getMetadata({
@@ -13,6 +15,12 @@ const Home: React.FC = () => {
     <main className="mt-6 -mx-3.5 md:mx-0 md:mt-10 rounded-xl bg-zinc-50 border border-zinc-200">
       <div className="h-[60vh] flex flex-col justify-center items-center gap-4">
         <p>🎉 Won the Aceternity UI Pro in Challenge 🎉</p>
+        <Link
+          className="text-sm text-zinc-600 underline underline-offset-2 hover:no-underline"
+          href="https://github.com/nirnejak/ui-animation-challenge"
+        >
+          View Source
+        </Link>
       </div>
     </main>
   )
