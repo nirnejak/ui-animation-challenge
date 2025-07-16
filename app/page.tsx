ca8be1c85a0420a2c52dd48d2607b6e0f35db5cc
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-07-16 20:42:40 +0530

    Home: thank you text updated

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,13 @@ const Home: React.FC = () => {
   return (
     <main className="mt-6 -mx-3.5 md:mx-0 md:mt-10 rounded-xl bg-zinc-50 border border-zinc-200">
       <div className="h-[60vh] flex flex-col justify-center items-center gap-4">
-        <p>🎉 Won the Aceternity UI Pro in Challenge 🎉</p>
+        <p className="text-3xl">🎉</p>
+        <p className="max-w-64 text-sm text-center">
+          Won the Aceternity UI Pro in the Challenge. Huuuge thanks to everyone
+          who supported!
+        </p>
         <Link
-          className="text-sm text-zinc-600 underline underline-offset-2 hover:no-underline"
+          className="text-xs text-zinc-600 underline underline-offset-2 hover:no-underline"
           href="https://github.com/nirnejak/ui-animation-challenge"
         >
           View Source
