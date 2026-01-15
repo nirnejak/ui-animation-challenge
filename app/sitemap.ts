c85aa3fc1548687e64c47cf53cb76f6a34e1b09c
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2026-01-15 12:08:57 +0530

    sitemap updated

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -13,7 +13,31 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 1,
     },
     {
-      url: baseUrl + `/contact/`,
+      url: baseUrl + `/day-1/`,
+      lastModified: new Date(),
+      changeFrequency: "daily" as const,
+      priority: 1,
+    },
+    {
+      url: baseUrl + `/day-2/`,
+      lastModified: new Date(),
+      changeFrequency: "daily" as const,
+      priority: 1,
+    },
+    {
+      url: baseUrl + `/day-3/`,
+      lastModified: new Date(),
+      changeFrequency: "daily" as const,
+      priority: 1,
+    },
+    {
+      url: baseUrl + `/day-4/`,
+      lastModified: new Date(),
+      changeFrequency: "daily" as const,
+      priority: 1,
+    },
+    {
+      url: baseUrl + `/day-5/`,
       lastModified: new Date(),
       changeFrequency: "weekly" as const,
       priority: 0.8,
